0f47f83ef040a63e2a0e445ca85eac2f1a79d0ed
gmrcWP <jorgesick.roch@gmail.com>
2025-09-16 14:59:21 -0400

    Mejoras presentacion, texto, colores y animaciones de habilidades

--- src/app/components/education/education.ts
+++ src/app/components/education/education.ts
@@ -11,22 +11,16 @@ import { CommonModule } from '@angular/common';
 export class Education {
   educationList = [
     {
-      degree: 'Bachelor of Science in Computer Science',
-      institution: 'University of Technology',
-      description: 'Focused on software engineering, data structures, algorithms, and web development. Graduated with honors and participated in various coding competitions.',
-      period: '2018 - 2022'
+      degree: 'Técnico Superior en Sistemas Informáticos',
+      institution: 'Instituto Técnico Jesus María',
+      //description: 'Focused on software engineering, data structures, algorithms, and web development. Graduated with honors and participated in various coding competitions.',
+      period: '2020 - 2023'
     },
     {
-      degree: 'Full Stack Web Development Bootcamp',
-      institution: 'Code Academy',
-      description: 'Intensive 6-month program covering modern web development technologies including React, Node.js, MongoDB, and deployment strategies.',
-      period: '2022'
+      degree: 'Curso de fullstack',
+      institution: 'Fullstack Open | Universidad de Helsinki',
+      //description: 'Intensive 6-month program covering modern web development technologies including React, Node.js, MongoDB, and deployment strategies.',
+      period: '2024'
     },
-    {
-      degree: 'High School Diploma',
-      institution: 'Central High School',
-      description: 'Completed with distinction in Mathematics and Computer Science. Active member of the programming club and robotics team.',
-      period: '2014 - 2018'
-    }
   ];
 }
